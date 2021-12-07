f6cce67458da26d60a15a981c4e4d78b989be2af
Natx113 <95668868+Natx113@users.noreply.github.com>
2021-12-06 22:03:20 -0600

    Add files via upload

---
+++ plantillas/formulario.php
@@ -0,0 +1,12 @@
+<form method="POST">
+    <section class="form-register">
+        <h4>Formulario de registro</h4>
+        <input class="controls" type="text" name="nombre" id="nombre" placeholder="ingrese su nombre">
+        <input class="controls" type="email" name="email" id="email" placeholder="ingrese su email">
+        <input class="controls" type="password" name="clave1" id="password" placeholder="ingrese su contraseña">
+        <input class="controls" type="password" name="clave2" id="password" placeholder="repita la contraseña">
+        <p>Estoy de acuerdo con <a href="terminos.php">Terminos y Condiciones</a></p>
+        <button class="botons" type="submit" name="enviar">Resgistrarse</button>
+        <p><a href="#">¿Ya tengo cuenta?</a></p>
+    </section>
+</form>
---
+++ plantillas/navbar.php
@@ -0,0 +1,15 @@
+
+<header>
+    <div class="menu">
+        <img class="logo" src="img/logo.png" alt="logo">
+       <nav>
+            <ul>
+                <li><a href="principal.php">Inicio</a></li>
+                <li><a href="registro.php">Registrarse</a></li>
+                <li><a href="DB.php">Base</a></li>
+               
+                
+            </ul>
+       </nav>
+    </div>
+</header>
